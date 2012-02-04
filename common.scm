52a1fe1be0c51bf9b915d6481d8f9507e8a34755
Ken Haase <kh@beingmeta.com>
2012-02-04 11:36:23 -0500

    Updated implementation

--- common.scm
+++ common.scm
@@ -7,6 +7,7 @@
 
 (define waiting.html (get-component "waiting.html"))
 (define enter.html (get-component "enter.html"))
+(define prepare.html (get-component "prepare.html"))
 
 (define-init sql/typemap #[])
 
@@ -32,25 +33,87 @@
       (if (uuid? input) (sql/getspace input)
 	  (error "Bad space identifier"))))
 
+(define sql/newspace
+  (extdb/proc sqldb
+    "INSERT INTO SPACES (uuid,name,about) VALUES (?,?,?)"
+    sql/typemap))
+(define sql/newspace+mural
+  (extdb/proc sqldb
+    "INSERT INTO SPACES (name,uuid,about,mural) VALUES (?,?,?,?)"
+    sql/typemap))
+(define (newspace name (uuid (getuuid)) (about #f) (mural #f))
+  (if mural
+      (sql/newspace+mural name uuid (qc (or about {})) mural)
+      (sql/newspace name uuid (qc (or about {}))))
+  uuid)
+
 (define sql/getministers
   (extdb/proc sqldb
     "SELECT minister FROM ministers WHERE space=?"
     `(%MERGE . ,sql/typemap)))
 (define (getministers space) (sql/getministers (get space 'uuid)))
 
-(define sql/getmessage
+(define sql/addminister
   (extdb/proc sqldb
-    "SELECT message
-      FROM messages
-     WHERE space=?
-      AND moment>?
- ORDER BY moment DESC
-    LIMIT 1"
-    `(%MERGE . ,sql/typemap)))
+    "INSERT INTO ministers (space,minister,joined) VALUES (?,?,?)"
+    sql/typemap))
+(define sql/dropminister
+  (extdb/proc sqldb
+    "DELETE FROM ministers WHERE space=? and minister=?"
+    sql/typemap))
+
+(define (enter space minister (uid #f))
+  (sql/addminister (get space 'uuid) minister))
+(define (leave space minister (uid #f))
+  (sql/dropminister (get space 'uuid) minister))
+
+(define sql/addmessage
+  (extdb/proc sqldb
+    "INSERT INTO messages
+                 (msgid,minister,content,spoken)
+          VALUES (?,?,?,?)"
+    sql/typemap))
+
+(define sql/lastmessage
+  (extdb/proc sqldb
+    "SELECT * FROM messages
+      WHERE space=?
+        AND shared IS NOT NULL
+   ORDER BY shared DESC
+      LIMIT 1"
+    sql/typemap))
+(define sql/nextmessage
+  (extdb/proc sqldb
+    "SELECT * FROM messages
+      WHERE space=?
+        AND shared IS NULL
+   ORDER BY spoken ASC
+      LIMIT 1"
+    sql/typemap))
+(define sql/lastmessage/since
+  (extdb/proc sqldb
+    "SELECT * FROM messages
+      WHERE space=?
+        AND shared IS NOT NULL
+        AND shared>?
+   ORDER BY shared DESC
+      LIMIT 1"
+    sql/typemap))
+
+(define sql/sharemessage
+  (extdb/proc sqldb
+    "UPDATE messages SET shared=? WHERE msgid=?"
+    sql/typemap))
+
 (define (getmessage space (interval 1200))
-  (sql/getmessage (get space 'uuid)
-		  (if (timestamp? interval) interval
-		      (timestamp+ (- interval)))))
+  (let ((tstamp (if (timestamp? interval) interval
+		    (timestamp+ (- interval)))))
+    (try (sql/lastmessage/since (get space 'uuid) tstamp)
+	 (let ((next (sql/nextmessage (get space 'uuid))))
+	   (sql/sharemessage (get next 'msgid) (gmtimestamp 'seconds))
+	   (sql/lastmessage (get space 'uuid))))))
+
+
 
 
 
--- ww.sql
+++ ww.sql
@@ -3,20 +3,24 @@ CREATE DATABASE ww character set=utf8 collate=utf8_general_ci;
 CREATE TABLE spaces (
    uuid BINARY(16) PRIMARY KEY,
    name VARCHAR(128) NOT NULL,
+   about TEXT,
    background VARCHAR(128),
    icon VARCHAR(128),
    knock VARCHAR(128));
 
 CREATE TABLE ministers (
-   space BINARY(16) NOT NULL,
+   space BINARY(16) NOT NULL REFERENCES spaces(uuid),
    minister VARCHAR(128) NOT NULL,
    joined DATETIME NOT NULL,
    uid INT(10));
-CREATE INDEX by_space on waiting(space);
+CREATE INDEX by_space on ministers(space);
 
 CREATE TABLE messages (
-   msgid INT(10) NOT NULL AUTO INCREMENT,
+   msgid BINARY(16) PRIMARY KEY,
+   space BINARY(16) NOT NULL REFERENCES spaces(uuid),
    minister VARCHAR(128) NOT NULL, uid INT(10),
-   moment DATETIME NOT NULL);
-CREATE INDEX msgs_by_space on messages(space);
+   content TEXT,
+   spoken DATETIME NOT NULL,
+   shared DATETIME);
+CREATE INDEX msgs_by_space on messages(wspace);
 
